378d0485945a1079287499f96a14bac5b80e6107
gonggangua <757385297@qq.com>
2022-12-12 01:26:47 +0800

    Add SpringBoot

--- main/src/main/java/com/config/WebConfig.java
+++ main/src/main/java/com/config/WebConfig.java
@@ -7,13 +7,11 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurationSupport {
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:static/");
-        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:templates/");
         super.addResourceHandlers(registry);
     }
 
