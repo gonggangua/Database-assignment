2ab1326b3db52f713ee2632c66447e06f31411c5
gonggangua <757385297@qq.com>
2022-12-11 19:34:32 +0800

    Add SpringBoot

---
+++ main/src/main/java/com/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+        super.addCorsMappings(registry);
+    }
+}
