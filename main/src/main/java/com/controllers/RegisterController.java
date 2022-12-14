9e53de6ed52c5a23a3f6692ef247699098917cb9
王俊哲 <20373232@buaa.edu.cn>
2022-12-14 17:13:40 +0800

    login and register interface

--- main/src/main/java/com/controllers/RegisterController.java
+++ main/src/main/java/com/controllers/RegisterController.java
@@ -1,6 +1,7 @@
 package com.controllers;
 
 import com.exceptions.DuplicateNameException;
+import com.interact.RetBody;
 import com.pojo.Register;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +12,15 @@ import java.util.Map;
 @RestController
 @RequestMapping("/register")
 public class RegisterController {
-    public String register(@RequestBody Map<String, Object> map) {
+    public Object register(@RequestBody Map<String, Object> map) {
         try {
             Register.register((String) map.get("username"), (String) map.get("password"), (String) map.get("mail"));
-            return "Register successfully.";
+            return new RetBody("Register successfully.");
         } catch (DuplicateNameException e) {
             e.printStackTrace();
-            return "Your name has been used.\nTry another one.";
+            RetBody retBody = new RetBody("Your name has been used.\nTry another one.");
+           retBody.addData("exist", "");
+            return retBody;
         }
     }
 }
--- main/src/main/java/com/controllers/UserController.java
+++ main/src/main/java/com/controllers/UserController.java
@@ -1,23 +1,31 @@
 package com.controllers;
 
 import com.exceptions.LoginFailException;
+import com.interact.RetBody;
 import com.pojo.Login;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/user")
+import java.util.Map;
+
+@RestController("/user")
 public class UserController {
     private Login login;
 
     @RequestMapping("/login")
-    public String login(@RequestParam("username") String username,
-                        @RequestParam("password") String password) {
+    public Object login(@RequestBody Map<String, Object> map) {
+        String username = (String) map.get("username");
+        String password = (String) map.get("password");
         try {
             login = new Login(username, password);
-            return "Login successfully.";
+            RetBody retBody = new RetBody("Login successful.");
+            retBody.addData("correct", "");
+            return retBody;
         } catch (LoginFailException e) {
             e.printStackTrace();
-            return "Login failed.";
+            return new RetBody("Login failed.");
         }
     }
 }
